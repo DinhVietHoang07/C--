4c0ea79f1aa3f9ad670159a918882501dfeaf3e4
DinhVietHoang07 <73330419+DinhVietHoang07@users.noreply.github.com>
2021-07-13 18:40:32 +0700

    Lên

---
+++ Task08/Mod2_Lab02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mod2_Lab02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
